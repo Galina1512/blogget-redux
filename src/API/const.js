a6b8b08086789297c176c28efb9167290109da59
Galina Kostina <Kostina13@ya.ru>
2024-01-29 09:57:31 +0300

    new redirect

--- src/API/const.js
+++ src/API/const.js
@@ -4,7 +4,7 @@ export const URL_AUTH = 'https://www.reddit.com/api/v1/authorize?';
 export const CLIENT_ID = 'yQBq7O1Ed6-GVZiYzRHdZg';
 export const RESPONSE_TYPE = 'token';
 export const RANDOM_STRING = 'random_string';
-export const REDIRECT_URI = 'https://blogget-redux.netlify/auth';
+export const REDIRECT_URI = 'https://blogget-redux.netlify.app/auth';
 export const SCOPE = 'identity submit read';
 
 // https://www.reddit.com/api/v1/authorize?client_id=CLIENT_ID&response_type=TYPE&
