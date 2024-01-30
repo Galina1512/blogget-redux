fcb16ac0fa919c00bc8688618560d8dc64520d6f
Galina Kostina <Kostina13@ya.ru>
2024-01-30 22:29:26 +0300

    redirects

--- src/API/const.js
+++ src/API/const.js
@@ -7,6 +7,3 @@ export const RANDOM_STRING = 'random_string';
 export const REDIRECT_URI = 'https://blogget-redux.netlify.app/auth';
 export const SCOPE = 'identity submit read';
 
-// https://www.reddit.com/api/v1/authorize?client_id=CLIENT_ID&response_type=TYPE&
-//     state=RANDOM_STRING&redirect_uri=URI&scope=SCOPE_STRING
-
